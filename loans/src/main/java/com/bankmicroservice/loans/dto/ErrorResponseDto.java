6ac42354843150fcf740d1b8566d2be72fcda453
Xeyrus <satyanand.tiwari.met13@iitbhu.ac.in>
2025-12-21 20:09:05 +0530

    Initial commit for accounts, cards and loans modules

---
+++ loans/src/main/java/com/bankmicroservice/loans/dto/ErrorResponseDto.java
@@ -0,0 +1,36 @@
+package com.bankmicroservice.loans.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data @AllArgsConstructor
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold Error response information"
+)
+public class ErrorResponseDto {
+
+    @Schema(
+            description = "API Path invoked by client"
+    )
+    private  String apiPath;
+
+    @Schema(
+            description = "Error code representing the error happening"
+    )
+    private HttpStatus errorCode;
+
+    @Schema(
+            description = "Error message representing the error happening"
+    )
+    private  String errorMessage;
+
+    @Schema(
+            description = "Time representing when the error happened"
+    )
+    private LocalDateTime errorTime;
+}
