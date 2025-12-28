efbb5d58a3f4530dd61be0f3f6f5a0fe764cb9e7
Xeyrus <satyanand.tiwari.met13@iitbhu.ac.in>
2025-12-28 21:22:24 +0530

    Added code for Phase 3 : Configuration management

---
+++ Phase 3 - Configuration Management/v1/loans/src/main/java/com/bankmicroservice/loans/entity/Loans.java
@@ -0,0 +1,32 @@
+package com.bankmicroservice.loans.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Loans extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanId;
+
+    private String mobileNumber;
+
+    private String loanNumber;
+
+    private String loanType;
+
+    private Long totalLoan;
+
+    private Long amountPaid;
+
+    private Long outstandingAmount;
+}
